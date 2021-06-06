8b9e5fabf2e326d513c150a7379ea15d00cc798a
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-06-06 20:59:54 +0530

    Create blink_led.ino

---
+++ blink_led.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode (D2, OUTPUT);
+
+}
+
+void loop() {                         //loop to blink the LED
+  for (int i=0; i<1024; i++){
+    analogWrite (D2, i);
+    delay(10);
+  }
+
+}
